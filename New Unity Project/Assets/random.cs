64ab61b8a7e6cfea0dc276337e3c95fb5274fb97
Juliano SF <JulianoSFA@github.com>
2020-01-06 20:32:29 -0300

    Início do menu de deletar

---
+++ New Unity Project/Assets/random.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class random : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    List<int> valueList;
+
+    void Start()
+    {
+        text.text = string.Empty;
+        valueList = new List<int>();
+        for (int i = 0; i < 20; i++)
+        {
+            
+            valueList.Add(RandomValue());
+        }
+
+        valueList.Sort();
+
+        for (int i = 0; i < valueList.Count; i++)
+        {
+            text.text += valueList[i].ToString() + " ";
+        }
+    }
+
+    int RandomValue()
+    {
+        int value = Random.Range(1, 60);
+        foreach (int v in valueList)
+        {
+            if (value == v)
+            {
+                RandomValue();
+            }
+        }
+        return value;
+    }
+}
